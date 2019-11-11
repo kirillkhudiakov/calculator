8880aa9dcf0b7d5d369dca1780a798e95e7d568a
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-11 17:12:56 +0300

    Add callback for click events

--- app/src/main/java/khudiakov/kirill/calculator/MainViewModel.kt
+++ app/src/main/java/khudiakov/kirill/calculator/MainViewModel.kt
@@ -6,26 +6,59 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    private var previous: Int? = null
-    private var number: Int = 0
+    private var previousNumber: Int? = null
+    private var previousOperator: String? = null
+    private var number: Int? = 0
 
     private val _result = MutableLiveData(0)
     val result: LiveData<Int>
         get() = _result
 
-    fun onButtonClicked(button: String) {
-        when {
-            button.isDigit() -> {
-                val digit = button.toInt()
-                number = number * 10 + digit
-                _result.value = number
-            }
+    private val _history = MutableLiveData<String>()
+    val history: LiveData<String>
+        get() = _history
 
+    fun onButtonClicked(symbol: String) {
+        when(symbol) {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" -> onDigitClicked(symbol)
+            "+", "-", "×", "÷", "%" -> onOperatorClicked(symbol)
+            "=" -> onEqualClicked()
+            "AC" -> onAllClearClicked()
+            "+/-" -> onPlusMinusClicked()
         }
     }
-}
 
-private fun String.isDigit(): Boolean {
-    return this == "0" || this == "1" || this == "2" || this == "3" || this == "4" || this == "5"
-            || this == "6" || this == "7" || this == "8" || this == "9"
+    private fun onDigitClicked(digit: String) {
+
+    }
+
+    private fun onOperatorClicked(operator: String) {
+
+    }
+
+    private fun onEqualClicked() {
+
+    }
+
+    private fun onAllClearClicked() {
+
+    }
+
+    private fun onPlusMinusClicked() {
+
+    }
+
+    private fun appendToHistory(symbol: String) {
+        _history.value = _history.value?.plus(symbol) ?: symbol
+    }
+
+    private fun calculate(a: Int, operator: String, b: Int): Int {
+        return when (operator) {
+            "+" -> a + b
+            "-" -> a - b
+            "×" -> a * b
+            "÷" -> a / b
+            else -> throw IllegalArgumentException("Unknown operator")
+        }
+    }
 }
